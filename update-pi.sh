5c5ccb42a38f286b1863de3fa2b04bbd60e22c72
Alex Forrence <aforren1@jhu.edu>
2016-06-05 11:13:50 -0400

    Added extra dependency, need to update with swap space creation (if it works)

--- update-pi.sh
+++ update-pi.sh
@@ -5,22 +5,24 @@
 # wget https://raw.githubusercontent.com/aforren1/rasppi-exp/master/update-pi.sh
 # also, make sure nonfree is uncommented in /etc/apt/sources.list, or the octave dependencies won't be grabbed
 
+# make a swap space?
+
 # basic update
 sudo apt-get -y --force-yes update
 sudo apt-get -y --force-yes upgrade
 
 # download octave, ptb dependencies
-sudo apt-get install -y git libportaudio-ocaml-dev subversion freeglut3 freeglut3-dev rhythmbox libusb-1.0 libdc1394-22-dev
+sudo apt-get install -y git portaudio19-dev libportaudio-ocaml-dev subversion freeglut3 freeglut3-dev rhythmbox libusb-1.0 libdc1394-22-dev
 
 sudo apt-get build-dep -y octave
-wget ftp://ftp.gnu.org/gnu/octave/octave-4.0.1.tar.gz
-tar xf octave-4.0.1.tar.gz
-cd octave-4.0.1/
+wget ftp://ftp.gnu.org/gnu/octave/octave-4.0.2.tar.gz
+tar xf octave-4.0.2.tar.gz
+cd octave-4.0.2/
 sudo ./configure
 sudo make
 sudo make install
 cd ..
-rm octave-4.0.1.tar.gz
+rm octave-4.0.2.tar.gz
 rm -rf ~/Documents
 rm -rf ~/Public
 rm -rf ~/Templates
