4864c9002a3ac163b3f1d8c33b8135bb1e62844f
Alex Forrence <aforren1@jhu.edu>
2016-07-07 10:13:53 -0400

    Little tweaks

--- update-pi.sh
+++ update-pi.sh
@@ -11,11 +11,13 @@ gpg --keyserver pgpkeys.mit.edu --recv-key 46925553
 gpg -a --export 46925553 | sudo apt-key add -
 gpg --keyserver pgpkeys.mit.edu --recv-key  2B90D010
 gpg -a --export 2B90D010 | sudo apt-key add -
-sudo apt-get -y --force-yes update
+sudo apt-get update
 sudo apt-get -y --force-yes upgrade
 
 # download ptb dependencies and octave (probably some redundancies)
 sudo apt-get install -y --force-yes git portaudio19-dev subversion freeglut3 freeglut3-dev rhythmbox libusb-1.0 libdc1394-22-dev libgl1-mesa-dri mesa-demos octave liboctave-dev
+# clean up accumulated waste
+sudo apt-get autoremove
 
 # turn on opengl from raspi-config, bump up video RAM to 256, and restart
 # check that opengl is working with `glxgears` (from mesa-demos)
